24ad46d98c0b231638af45343cedb2c2b0f03533
deathbreakfast2@gmail.com <deathbreakfast2@gmail.com>
2022-03-16 13:07:12 -0700

    Added debian script

---
+++ debian.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+apt update
+
+# tzdata is one the prerequisites, if it is not installed we want to install it silently.
+DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends tzdata
+
+apt install apt-transport-https ca-certificates curl software-properties-common -y
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+echo \
+	"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io -y
+
